a26ed0b3171e73e6f716fe4cac61b70eac79022c
cristiscu <cristiscu@gmail.com>
2023-08-30 20:08:09 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+Hierarchy Viewer
+================
+
+Generates a hierarchy with GraphViz for any parent-child relationship hidden in a CSV tabular data file, with column names as header row.
+
+(1) Simplified Graphs
+=====================
+
+Run the following from the command line, to show the simple employee-manager dependency hierarchy, with employee ID as default object identifiers. Remark that the "from" column must be the unique identifier for each row. These are all the requiered command line arguments:
+
+**<code>python viewer.py --f data/employees.csv --from employee_id --to manager_id</code>**
+
+![employee-manager-hierarchy](images/employee-manager-hierarchy.png)
+
+IDs are unique, but not very friendly. If you have a column that can be used to easily identify an object, you can mention it. Here we have the same hierarchy, but we indetify the employees by their names:
+
+**<code>python viewer.py --f data/employees.csv --from employee_id --to manager_id --d employee_name</code>**
+
+![hierarchy-with-employee-names](images/hierarchy-with-employee-names.png)
+
+The **--rev** switch will change the direction of each arrow from parent to child (by default it is from child to parent). Here is the manager-employee hierarchy:
+
+**<code>python viewer.py --f data/employees.csv --from employee_id --to manager_id --d employee_name --rev</code>**
+
+![manager-employee-hierarchy](images/manager-employee-hierarchy.png)
+
+(2) Graphs with Groups
+======================
+
+Same as before, except we added the department ID to group all employees from each department in a surrounding "box":
+
+**<code>python viewer.py --f data/employees.csv --from employee_id --to manager_id --d employee_name --g department_id</code>**
+
+![group-by-department](images/group-by-department.png)
+
+In a similar manner we may group employees by job title:
+
+**<code>python viewer.py --f data/employees.csv --from employee_id --to manager_id --d employee_name --g job_id</code>**
+
+![group-by-job-title](images/group-by-job-title.png)
+
+(3) Detailed Graphs
+===================
+
+The **--all** flag will "expand" all other properties (except the display name) from the CSV file for each object. If you get an error that the URL is too long for the browser, copy and paste the generated DOT file content into the online GraphViz editor (but be aware that this may generate indeed a lot of data!).
+
+**<code>python viewer.py --f data/employees.csv --from employee_id --to manager_id --d employee_name --all</code>**
+
+Here is a partial view of the generated graph:
+
+![show-details](images/show-details.png)
+
+*Note*: **employees.csv** test file has been adapter [from here](https://gist.github.com/kevin336/acbb2271e66c10a5b73aacf82ca82784).
---
+++ viewer.py
@@ -0,0 +1,129 @@
+"""
+Created By:    Cristian Scutaru
+Creation Date: Aug 2023
+Company:       XtractPro Software
+"""
+
+import sys, argparse, webbrowser, urllib.parse
+import pandas as pd
+
+def showUsage(msg=''):
+    if len(msg) > 0: print(msg)
+    print("Usage: python viewer.py options\n"
+        "--f filename        - [required] CSV file name\n"
+        "--from column-name  - [required] name of the source child relationship column (used as identifier)\n"
+        "--to column-name    - [required] name of the target parent relationship column (used as foreign key)\n"
+        "--rev               - [optional] when true, the arrow will be directed from parent to child\n"
+        "--d column-name     - [optional] name of a display column (or the 'from' column name will be used by default)\n"
+        "--g column-name     - [optional] name of a column to group objects by\n"
+        "--all               - [optional] expand all objects and show all related properties from the CSV file\n")
+    sys.exit(2)
+
+def processArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--f', dest='filename')
+    parser.add_argument('--from', dest='fromCol')
+    parser.add_argument('--to', dest='toCol')
+    parser.add_argument('--rev', dest='rev', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--d', dest='displayCol')
+    parser.add_argument('--g', dest='groupCol')
+    parser.add_argument('--all', dest='all', action=argparse.BooleanOptionalAction)
+    args = parser.parse_args()
+
+    if (args.filename == None
+        or args.fromCol == None
+        or args.toCol == None):
+        showUsage("You miss at least one required argument!")
+
+    return args
+
+def makeGraph(df, cols, fromCol, toCol, displayCol, groupCol, rev, all):
+    s = ""; t = ""
+
+    # add groups (as subgraph clusters)
+    if groupCol is not None:
+        df = df.sort_values(by=[groupCol])
+        t = "\t"
+
+    # add nodes
+    g = None; gps = 1;
+    for i, row in df.iterrows():
+        # read group and create subgroup cluster for next nodes
+        # see https://stackoverflow.com/questions/2012036/graphviz-how-to-connect-subgraphs
+        if groupCol is not None:
+            grp = str(row[groupCol])
+            if g is None or grp != g:
+                if g is not None: s += '\n\t}'
+                s += (f'\n\tsubgraph cluster{gps} {{\n'
+                    + f'\t\tlabel="{grp}"')
+                gps += 1; g = grp
+
+        # add node (with label)
+        label = str(row[fromCol]) if displayCol is None else str(row[displayCol])
+        display = f' [label="{label}"]'
+        if all:
+            # get and show all row properties in the "exploded" node
+            vals = ''
+            for col in cols:
+                if col != displayCol:
+                    val = '&nbsp;' if pd.isnull(row[col]) else str(row[col])
+                    vals += (f'\t\t<tr><td align="left"><font color="#000000">{col}&nbsp;</font></td>\n'
+                        + f'\t\t<td align="left"><font color="#000000">{val}</font></td></tr>\n')
+
+            display = (f' [ label=<<table style="rounded" border="0" cellborder="0" cellspacing="0" cellpadding="1">\n'
+                + f'\t\t<tr><td align="center" colspan="2"><font color="#000000"><b>{label}</b></font></td></tr>\n'
+                + f'{vals}\t\t</table>> ]')
+        s += f'\n\t{t}n{str(row[fromCol])}{display};'
+    if groupCol is not None and g is not None: s += '\n\t}'
+
+    # add links
+    for i, row in df.iterrows():
+        if not pd.isna(row[toCol]):
+            if rev: s += f'\n\tn{str(row[toCol])} -> n{str(row[fromCol])};'
+            else: s += f'\n\tn{str(row[fromCol])} -> n{str(row[toCol])};'
+
+    # add digraph around
+    s = (f'digraph d {{\n'
+        + f'\tgraph [rankdir="LR"; compound="True" color="Gray"];\n'
+        + f'\tnode [shape="Mrecord" style="filled" color="SkyBlue"]'
+        + f'{s}\n}}')
+    #print(s)
+    return s
+   
+def main():
+    args = processArgs()
+
+    df = pd.read_csv(args.filename).convert_dtypes()
+    cols = list(map(str.upper, df.columns.values.tolist()))
+    df = df.reset_index()  # make sure indexes pair with number of rows
+
+    # validate column names
+    fromCol = args.fromCol.upper()
+    if fromCol not in cols: showUsage("'from' column not found!")
+    
+    toCol = args.toCol.upper()
+    if toCol not in cols: showUsage("'to' column not found!")
+    
+    displayCol = None
+    if args.displayCol is not None:
+        displayCol = args.displayCol.upper()
+        if displayCol not in cols: showUsage("'display' column not found!")
+    
+    groupCol = None
+    if args.groupCol is not None:
+        groupCol = args.groupCol.upper()
+        if groupCol not in cols: showUsage("'group' column not found!")
+
+    # make graph
+    s = makeGraph(df, cols, fromCol, toCol, displayCol, groupCol, args.rev, args.all)
+    with open(f"{args.filename}.dot", "w") as file:
+        file.write(s)
+
+    # url-encode as query string for remote Graphviz Visual Editor
+    s = urllib.parse.quote(s)
+    s = f'http://magjac.com/graphviz-visual-editor/?dot={s}'
+    # print(s)
+    webbrowser.open(s)
+
+if __name__ == '__main__':
+  main()
